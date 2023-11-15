4d3264342f7a37042d5b52471c90357fc5590ee8
seanly liu <seanly@opsbox.dev>
2023-11-15 08:46:26 +0800

    update config files list show method

--- src/main/java/cn/opsbox/jenkinsci/plugins/cps/ConfigFileProviderOesTemplateFlowDefinitionConfiguration.java
+++ src/main/java/cn/opsbox/jenkinsci/plugins/cps/ConfigFileProviderOesTemplateFlowDefinitionConfiguration.java
@@ -1,11 +1,20 @@
 package cn.opsbox.jenkinsci.plugins.cps;
 
+import com.cloudbees.plugins.credentials.common.StandardListBoxModel;
 import hudson.Extension;
+import hudson.util.ListBoxModel;
 import lombok.Getter;
+import lombok.SneakyThrows;
+import org.apache.commons.lang.StringUtils;
+import org.jenkinsci.lib.configprovider.model.Config;
+import org.jenkinsci.plugins.configfiles.ConfigFileStore;
+import org.jenkinsci.plugins.configfiles.GlobalConfigFiles;
 import org.kohsuke.stapler.DataBoundConstructor;
 
+import java.util.Collection;
+
 public class ConfigFileProviderOesTemplateFlowDefinitionConfiguration extends OesTemplateFlowDefinitionConfiguration {
- 
+
     @Getter
     private final String scriptId;
 
@@ -18,6 +27,24 @@ public class ConfigFileProviderOesTemplateFlowDefinitionConfiguration extends Oe
     public static class DescriptorImpl extends OesTemplateFlowDefinitionConfiguration.DescriptorImpl {
         public String getDisplayName() {
             return "From Config File";
+        }
+
+        @SneakyThrows
+        public ListBoxModel doFillScriptIdItems() {
+
+            ListBoxModel items = new ListBoxModel();
+            try {
+                ConfigFileStore store = GlobalConfigFiles.get();
+                Collection<Config> configs = store.getConfigs();
+                for (Config config : configs) {
+                  items.add(config.name, config.id);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+                return new StandardListBoxModel().includeEmptyValue();
+            }
+
+            return items;
         }
     }
 }
